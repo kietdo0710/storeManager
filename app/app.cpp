209e8a6cc3fe1811e8de55ec64b3f8ea09de0e7d
kietdo <doanhkiet1091@gmail.com>
2026-02-21 11:04:56 -0800

    Add set price popup

--- app/app.cpp
+++ app/app.cpp
@@ -1,5 +1,6 @@
 #include "clock/clock.hpp"
 #include "ui/ui.hpp"
+#include "popup.hpp"
 #include <ncurses.h>
 #include <iostream>
 #include <cstring>
@@ -10,27 +11,48 @@ using namespace std;
 // Global variables.
 WINDOW* ue1;
 WINDOW* desc;
+WINDOW* debug;
 bool halt = false;
-bool sw1 = false;
-bool sw2 = false;
-bool sw3 = false;
-bool sw4 = false;
-bool sw5 = false;
-bool sw6 = false;
-bool sw7 = false;
+bool setp_display = false;
+int debug_key;
+
 
 Clock* globalClock = new Clock();
 Ui* myUi = new Ui();
 
-void update_display();
+SetPricePopup *set_price_popup = NULL;
+
+void store_display();
+void event_display();
+void debug_display();
+
+void set_price_display();
 
 void program()
 {
 	while (1)
 	{
-		update_display();
+		event_display();
+		store_display();
+		debug_display();
+		// set_price_display();
 		std::this_thread::sleep_for(std::chrono::milliseconds(100));
 	}
+}
+
+void debug_display()
+{
+	// string cont = "";
+	// cont.push_back(debug_key);
+	mvwprintw(debug, 2, 2, "Last input:    ");
+	mvwprintw(debug, 2, 2, "Last input: %d", debug_key);
+	wrefresh(debug);	// Show the box.
+}
+
+void event_display()
+{
+	mvwprintw(desc, 2, 9, "    ");
+	wrefresh(desc);	// Show the box.
 }
 
 void display_basic_ui(void)
@@ -42,20 +64,22 @@ void display_basic_ui(void)
 	
 	mvwprintw(ue1, 0, 2, " Store ");
 	mvwprintw(ue1, 2, 2, ">> Stocks <<");
-	mvwprintw(ue1, 3, 2, "Fish :            Bean :");
-	mvwprintw(ue1, 4, 2, "Beef :            Vegie:");
+	// mvwprintw(ue1, 3, 2, "Fish :            Bean :");
+	// mvwprintw(ue1, 4, 2, "Beef :            Vegie:");
 	
-	mvwprintw(ue1, 6, 2, ">> Market price <<");
-	mvwprintw(ue1, 7, 2, "Fish :            Bean :");
-	mvwprintw(ue1, 8, 2, "Beef :            Vegie:");
+	mvwprintw(ue1, 6, 2, ">> Price <<");
+	mvwprintw(ue1, 7, 2, "Fish :           Bean :");
+	// mvwprintw(ue1, 8, 2, "Beef :            Vegie:");
 	
 	mvwprintw(ue1, 25, 2, "Processor is: RUNNING");
+	wrefresh(desc);	// Show the box.
+	wrefresh(ue1);	// Show the box.
 }
 
 
 // --- UPDATE DISPLAY ---
 // Update the status display.
-void update_display()
+void store_display()
 {
 	char time[20]; 
 	globalClock->getTime(time);
@@ -66,10 +90,25 @@ void update_display()
 	mvwprintw(ue1, 3, 2, "Fish : %2d        Bean : %2d",12,34);
 	mvwprintw(ue1, 4, 2, "Beef : %2d        Vegie: %2d",56,78);
 	
-	mvwprintw(ue1, 6, 2, ">> Market price <<");
-	mvwprintw(ue1, 7, 2, "Fish : %2d        Bean : %2d",11,22);
+	mvwprintw(ue1, 6, 2, ">> Price <<");
+	// Fish price 
+	wattron(ue1, COLOR_PAIR(3) | A_BOLD);
+	mvwprintw(ue1, 7, 9, "%2d",11);
+	wattroff(ue1, COLOR_PAIR(3) | A_BOLD);
+	wattron(ue1, COLOR_PAIR(2) | A_BOLD);
+	mvwprintw(ue1, 7, 12, "(%2d)",11);
+	wattroff(ue1, COLOR_PAIR(2) | A_BOLD);
+
+	// Bean price 
+	wattron(ue1, COLOR_PAIR(3) | A_BOLD);
+	mvwprintw(ue1, 7, 26, "%2d",11);
+	wattroff(ue1, COLOR_PAIR(3) | A_BOLD);
+	wattron(ue1, COLOR_PAIR(2) | A_BOLD);
+	mvwprintw(ue1, 7, 29, "(%2d)",11);
+	wattroff(ue1, COLOR_PAIR(2) | A_BOLD);
+
+
 	mvwprintw(ue1, 8, 2, "Beef : %2d        Vegie: %2d",33,44);
-	mvwprintw(desc, 2, 9, "    ");
 	
 	if (halt) {
 		wattron(ue1, COLOR_PAIR(2) | A_BOLD);
@@ -83,7 +122,6 @@ void update_display()
 		wattroff(ue1, COLOR_PAIR(3) | A_BOLD);
 		globalClock->start();
 	}
-	wrefresh(desc);	// Show the box.
 	wrefresh(ue1);	// Show the box.
 }
 
@@ -116,10 +154,10 @@ int main ()
 	// arguments: rows, columns, start_y, start_x
 	desc = newwin(9, 70, 1, 2);
 	ue1 = newwin(28, 60, 10, 2);
+	debug = newwin(5, 20, 38, 2);
 	box(desc, 0, 0);	// use default window outline characters.
 	box(ue1, 0, 0);		// use default window outline characters.
-	wrefresh(desc);	// Show the box.
-	wrefresh(ue1);	// Show the box.
+	box(debug, 0, 0);		// use default window outline characters.
 	
 	// Use a panel to ease management. 
 	//PANEL* desc_panel = new_panel(desc);
@@ -130,18 +168,31 @@ int main ()
 	thread t1 = thread(&program);
 	t1.detach();
 	while (true) {
-		wrefresh(desc);	// Show the box.
-		wrefresh(ue1);	// Show the box.
-		
 		key = wgetch(desc);
-		if (key == 'q') {
-			break;
+		debug_key = key;
+		if (set_price_popup == NULL)
+		{
+			if (key == 'q') {
+				break;
+			}
+			else if (key == 'h') {
+				halt = true;
+			}
+			else if (key == 'g') {
+				halt = false;
+			}
+			else if (key == 'f')
+			{
+				set_price_popup = new SetPricePopup(5, 35, 20, 20, "fish"); 
+			}
 		}
-		else if (key == 'h') {
-			halt = true;
-		}
-		else if (key == 'g') {
-			halt = false;
+		else
+		{
+			if (0 != set_price_popup->input(key))
+			{
+				// Object no longer exist
+				set_price_popup = NULL;
+			}
 		}
 	}
 
